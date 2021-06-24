12865111400b24a2ec98851ca93c720075f10fa1
Thomas Richards <thomas@mohc.net>
2021-06-24 09:56:31 +0100

    docs: add comments to exports

--- money.go
+++ money.go
@@ -2,11 +2,13 @@ package httpshopify
 
 import "github.com/MOHC-LTD/shopify"
 
+// MoneyDTO represents a quantity of money in HTTP requests and responses
 type MoneyDTO struct {
 	Amount       string `json:"amount"`
 	CurrencyCode string `json:"currency_code"`
 }
 
+// ToShopify converts this DTO to the Shopify equivalent
 func (dto MoneyDTO) ToShopify() shopify.Money {
 	return shopify.Money{
 		Amount:       dto.Amount,
--- tax-line.go
+++ tax-line.go
@@ -2,13 +2,16 @@ package httpshopify
 
 import "github.com/MOHC-LTD/shopify"
 
+// TaxLineDTOs is a collection of tax line DTOs
 type TaxLineDTOs []TaxLineDTO
 
+// TaxLineDTO represents a Shopify tax line in HTTP requests and responses
 type TaxLineDTO struct {
 	Title string  `json:"title"`
 	Rate  float32 `json:"rate"`
 }
 
+// ToShopify converts this DTO to the Shopify equivalent
 func (dto TaxLineDTO) ToShopify() shopify.Tax {
 	return shopify.Tax{
 		Title: dto.Title,
