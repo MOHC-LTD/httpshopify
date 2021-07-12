0a3c161ea651ffced6d33e8a0310ee71d16e30fb
Harry Withers <harry@mohc.net>
2021-07-12 13:06:01 +0100

    reactor: http package to use single method base

--- internal/http/client.go
+++ internal/http/client.go
@@ -1,6 +1,10 @@
 package http
 
-import "net/http"
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+)
 
 // Client is a HTTP client
 type Client struct {
@@ -25,4 +29,35 @@ func (client Client) AppendDefaultHeaders(headers RequestHeaders) RequestHeaders
 	}
 
 	return headers
+}
+
+// Do does a request
+func (client Client) Do(method string, url string, headers RequestHeaders, body io.Reader) ([]byte, ResponseHeaders, error) {
+	headers = client.AppendDefaultHeaders(headers)
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, ResponseHeaders{}, err
+	}
+
+	for _, header := range headers {
+		req.Header.Set(header.Name, header.Value)
+	}
+
+	resp, err := client.Client.Do(req)
+	if err != nil {
+		return nil, ResponseHeaders{}, err
+	}
+
+	err = HandleStatus(resp.StatusCode)
+	if err != nil {
+		return nil, ResponseHeaders{}, err
+	}
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, ResponseHeaders{}, err
+	}
+
+	return responseBody, ResponseHeaders{resp.Header}, nil
 }
