09227509de5681fa38583401054f1f54ab2394c3
Harry Withers <harry@mohc.net>
2021-07-12 13:24:53 +0100

    feat: rate limited http client

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/MOHC-LTD/shopify v1.9.0
-	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6 // indirect
+	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6
 )
--- internal/http/client.go
+++ internal/http/client.go
@@ -1,23 +1,33 @@
 package http
 
 import (
+	"context"
 	"io"
 	"io/ioutil"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
 
 // Client is a HTTP client
 type Client struct {
 	*http.Client
 	defaultHeaders RequestHeaders
+	limiter        *rate.Limiter
 }
 
 // NewClient builds a new HTTP client
-func NewClient(defaultHeaders RequestHeaders) Client {
-	return Client{
-		&http.Client{},
-		defaultHeaders,
+func NewClient(defaultHeaders RequestHeaders, options ...Option) Client {
+	client := Client{
+		Client:         &http.Client{},
+		defaultHeaders: defaultHeaders,
 	}
+
+	for _, option := range options {
+		option.configure(&client)
+	}
+
+	return client
 }
 
 // AppendDefaultHeaders appends the default headers to the passed ones.
@@ -33,6 +43,11 @@ func (client Client) AppendDefaultHeaders(headers RequestHeaders) RequestHeaders
 
 // Do does a request
 func (client Client) Do(method string, url string, headers RequestHeaders, body io.Reader) ([]byte, ResponseHeaders, error) {
+	if client.limiter != nil {
+		ctx := context.Background()
+		client.limiter.Wait(ctx)
+	}
+
 	headers = client.AppendDefaultHeaders(headers)
 
 	req, err := http.NewRequest(method, url, body)
