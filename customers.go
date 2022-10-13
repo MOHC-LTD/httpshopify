1acf0def80b2fd3e0cc7a55ff1885d33a22aa87e
rob-mohc <robert.giurgiulescu@mohc.net>
2022-10-13 11:57:55 +0100

    refactor: removed comment

--- customers.go
+++ customers.go
@@ -188,7 +188,6 @@ func (c customerRepository) GetByQuery(fields []string, query string) (shopify.C
 
 	body, _, err := c.client.Get(url, nil)
 	if err != nil {
-		// TODO This ErrHTTP feels like bloat now. Can probably simplify the http work
 		switch err.(http.ErrHTTP).Code {
 		case httpCode.StatusNotFound:
 			return shopify.Customers{}, shopify.NewErrCustomerSearchNotFound(query)
