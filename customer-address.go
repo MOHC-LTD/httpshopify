1dba46514f575df91f20646b2ce9e62f11b16a43
Eze Lopes <85875479+ezelopes-mohc@users.noreply.github.com>
2022-10-26 18:02:45 +0100

    feat(SHP-545): Create new address (#92)
    * feat: create new address
    
    * feat: update tags when updating customer
    
    * chore: customer address id
    
    * ci: shopify bump version

--- customer-address.go
+++ customer-address.go
@@ -1,9 +1,11 @@
 package httpshopify
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/MOHC-LTD/httpshopify/internal/http"
+	"github.com/MOHC-LTD/shopify"
 )
 
 type customerAddressRepository struct {
@@ -16,6 +18,41 @@ func newCustomerAddressRepository(client http.Client, createURL func(endpoint st
 		client,
 		createURL,
 	}
+}
+
+func (r customerAddressRepository) Create(id int64, address shopify.CustomerAddress) (shopify.CustomerAddress, error) {
+	url := r.createURL(fmt.Sprintf("customers/%v/addresses.json", id))
+
+	addressDTO := BuildCustomerAddressDTO(address)
+
+	// Wrapping Address object inside `address` property
+	request := struct {
+		CustomerAddressDTO `json:"address"`
+	}{
+		addressDTO,
+	}
+
+	body, err := json.Marshal(request)
+	if err != nil {
+		return shopify.CustomerAddress{}, err
+	}
+
+	resBody, _, err := r.client.Post(url, body, nil)
+	if err != nil {
+		return shopify.CustomerAddress{}, err
+	}
+
+	// Wrapping Address object inside `address` property
+	responseDTO := struct {
+		CustomerAddressDTO `json:"customer_address"`
+	}{}
+
+	err = json.Unmarshal(resBody, &responseDTO)
+	if err != nil {
+		return shopify.CustomerAddress{}, err
+	}
+
+	return responseDTO.ToShopify(), nil
 }
 
 func (r customerAddressRepository) Delete(id int64, addressID int64) error {
