aad5ea7bee0ca7e7158ab8c64a2216d4d89c5966
robmohc <robert.giurgiulescu@mohc.net>
2023-03-14 09:51:45 +0000

    feat: set customer default address

--- customer-address.go
+++ customer-address.go
@@ -88,6 +88,39 @@ func (r customerAddressRepository) Update(id int64, address shopify.CustomerAddr
 	return responseDTO.ToShopify(), nil
 }
 
+func (r customerAddressRepository) SetDefault(id int64, address shopify.CustomerAddress) (shopify.CustomerAddress, error) {
+	addressDTO := BuildCustomerAddressDTO(address)
+
+	request := struct {
+		CustomerAddressDTO `json:"address"`
+	}{
+		addressDTO,
+	}
+
+	body, err := json.Marshal(request)
+	if err != nil {
+		return shopify.CustomerAddress{}, err
+	}
+
+	url := r.createURL(fmt.Sprintf("customers/%v/addresses/%v/default.json", id, address.ID))
+
+	resBody, _, err := r.client.Put(url, body, nil)
+	if err != nil {
+		return shopify.CustomerAddress{}, err
+	}
+
+	responseDTO := struct {
+		CustomerAddressDTO `json:"customer_address"`
+	}{}
+
+	err = json.Unmarshal(resBody, &responseDTO)
+	if err != nil {
+		return shopify.CustomerAddress{}, err
+	}
+
+	return responseDTO.ToShopify(), nil
+}
+
 func (r customerAddressRepository) Delete(id int64, addressID int64) error {
 	url := r.createURL(fmt.Sprintf("customers/%v/addresses/%v.json", id, addressID))
 
